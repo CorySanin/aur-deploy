a3257f94401ed606c65fde65fcb2c4d0962bc1e6
Cory Sanin <corysanin@outlook.com>
2025-11-21 10:35:19 -0500

    fix pkgrel sed command

--- commit.sh
+++ commit.sh
@@ -6,7 +6,7 @@ git pull || exit 1
 git clean -fdx
 if [[ `git status --porcelain` ]]; then
     echo "==> Update detected"
-    sed -i -e 's|pkgrel=.*|pkgrel=1|g' ./PKGBUILD
+    sed -i -e 's|^pkgrel=.*|pkgrel=1|g' ./PKGBUILD
     makepkg --printsrcinfo > .SRCINFO
     git diff ./PKGBUILD
     git add PKGBUILD .SRCINFO
